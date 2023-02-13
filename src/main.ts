ff901d369c5507d5faa0e57d62f3ad3f6ae12109
David Garcia <Davidgarciama@outlook.com>
2023-02-13 13:39:42 -0600

    Make code readable and easily maintainable

--- src/main.ts
+++ src/main.ts
@@ -1,174 +1,70 @@
-// Short Intro
-
-// Some of you might remember spending afternoons playing Street Fighter 2 in some Arcade back in the 90s or emulating it nowadays with the numerous emulators for retro consoles.
-
-// Can you solve this kata? Suuure-You-Can!
-
-// UPDATE: a new kata's harder version is available here.
-
-// The Kata
-
-// You'll have to simulate the video game's character selection screen behaviour, more specifically the selection grid. Such screen looks like this:
-
-// Screen:
-
-// screen
-
-// Selection Grid Layout in text:
-
-// | Ryu  | E.Honda | Blanka  | Guile   | Balrog | Vega    |
-// | Ken  | Chun Li | Zangief | Dhalsim | Sagat  | M.Bison |
-// Input
-
-// the list of game characters in a 2x6 grid;
-// the initial position of the selection cursor (top-left is (0,0));
-// a list of moves of the selection cursor (which are up, down, left, right);
-// Output
-
-// the list of characters who have been hovered by the selection cursor after all the moves (ordered and with repetition, all the ones after a move, whether successful or not, see tests);
-// Rules
-
-// Selection cursor is circular horizontally but not vertically!
-
-// As you might remember from the game, the selection cursor rotates horizontally but not vertically; that means that if I'm in the leftmost and I try to go left again I'll get to the rightmost (examples: from Ryu to Vega, from Ken to M.Bison) and vice versa from rightmost to leftmost.
-
-// Instead, if I try to go further up from the upmost or further down from the downmost, I'll just stay where I am located (examples: you can't go lower than lowest row: Ken, Chun Li, Zangief, Dhalsim, Sagat and M.Bison in the above image; you can't go upper than highest row: Ryu, E.Honda, Blanka, Guile, Balrog and Vega in the above image).
-
-// Test
-
-// For this easy version the fighters grid layout and the initial position will always be the same in all tests, only the list of moves change.
-
-// Notice: changing some of the input data might not help you.
-
-// Examples
-
-// fighters = [
-//   ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
-//   ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
-// ]
-// initial_position = (0,0)
-// moves = ['up', 'left', 'right', 'left', 'left']
-// then I should get:
-
-// ['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']
-// as the characters I've been hovering with the selection cursor during my moves. Notice: Ryu is the first just because it "fails" the first up See test cases for more examples.
-
-// fighters = [
-//   ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
-//   ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
-// ]
-// initial_position = (0,0)
-// moves = ['right', 'down', 'left', 'left', 'left', 'left', 'right']
-// Result:
-
-// ['E.Honda', 'Chun Li', 'Ken', 'M.Bison', 'Sagat', 'Dhalsim', 'Sagat']
-// OFF-TOPIC
-
 type Move = 'down' | 'up' | 'right' | 'left'
 
-
-let streetFighterSelection = (fighters: string[][], position: number[], moves: Move[]) => {
+const streetFighterSelection = (fighters: string[][], position: number[], moves: Move[]):string[] => {
     let hoverChar: string[] = [];
-    moves.forEach((move) => {
+
+    moves.forEach((move:Move) => {
+        //Create a switch to interact with position array, the idea is to have a coordenates system
         switch (move) {
             case 'down':
-                if (position[0] == 0) {
-                    position[0] += 1
-                }
+                if (position[0] == 0) position[0] += 1;
                 break;
             case 'up':
-                if (position[0] == 1) {
-                    position[0] -= 1
-                }
+                if (position[0] == 1) position[0] -= 1
                 break;
             case 'right':
-                if (position[1] == 5) {
-                    position[1] = 0
-                } else {
-                    position[1] += 1
-                }
+                position[1] == 5?position[1] = 0:position[1] += 1
                 break;
             case 'left':
-                if (position[1] == 0) {
-                    position[1] = 5
-                } else {
-                    position[1] -= 1
-                }
+                position[1] == 0?position[1] = 5:position[1] -= 1
                 break
         }
-        //arriba
-        if (position[0] == 0) {
-            if (position[1] == 0) {
-                hoverChar.push(fighters[0][0])
-            } else if (position[1] == 1) {
-                hoverChar.push(fighters[0][1])
-            } else if (position[1] == 2) {
-                hoverChar.push(fighters[0][2])
-            }
-            else if (position[1] == 3) {
-                hoverChar.push(fighters[0][3])
-            } else if (position[1] == 4) {
-                hoverChar.push(fighters[0][4])
-            } else if (position[1] == 5) {
-                hoverChar.push(fighters[0][5])
-            }
 
+        //Time to save every character we "visited" (by hovering), each movement it registers the character on the position that you are 
+        //First file of characters
+        if (position[0] == 0) {
+            fighters[0].forEach((fighter, index:number) => {
+                if (position[1] == index) {
+                    hoverChar.push(fighters[0][index])
+                }
+            })
         }
-        //abajo
+        //Second file of characters
         else {
-            if (position[1] == 0) {
-                hoverChar.push(fighters[1][0])
-            } else if (position[1] == 1) {
-                hoverChar.push(fighters[1][1])
-            }
-            else if (position[1] == 2) {
-                hoverChar.push(fighters[1][2])
-            }
-            else if (position[1] == 3) {
-                hoverChar.push(fighters[1][3])
-            }
-            else if (position[1] == 4) {
-                hoverChar.push(fighters[1][4])
-            } else if (position[1] == 5) {
-                hoverChar.push(fighters[1][5])
-            }
+            fighters[1].forEach((fighter, index:number) => {
+                if (position[1] == index) {
+                    hoverChar.push(fighters[1][index])
+                }
+            })
         }
-        hoverChar.push()
 
     })
-    console.log(hoverChar)
     return hoverChar
-    
-
 }
 
-// moves = ["up","left","down","right","up","left","down","right"];
+// Testing
 
-let fighters: string[][] = [
-    ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
-    ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
-];
-let opts: Move[] = ["up", "down", "right", "left"]
-let moves: Move[] = []
+// let fighters: string[][] = [
+//     ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+//     ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+// ];
+// let moves: Move[] = []
 
 
-// streetFighterSelection(fighters, [0, 0], moves)//,[];
+// console.log(streetFighterSelection(fighters, [0, 0], moves))   //,[];
 
 // moves = ['up', 'left', 'right', 'left', 'left'];
-// streetFighterSelection(fighters, [0, 0], moves)//,['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']);
-
-
-
+// console.log(streetFighterSelection(fighters, [0, 0], moves))//,['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']);
 
 // moves = ["left", "left", "left", "left", "left", "left", "left", "left"];
-// streetFighterSelection(fighters, [0, 0], moves)//,['Vega', 'Balrog', 'Guile', 'Blanka', 'E.Honda', 'Ryu', 'Vega', 'Balrog'];
+// console.log(streetFighterSelection(fighters, [0, 0], moves))//,['Vega', 'Balrog', 'Guile', 'Blanka', 'E.Honda', 'Ryu', 'Vega', 'Balrog'];
 
 
 // moves = ['up', 'down', 'right', 'up', 'left'];
-// streetFighterSelection(fighters, [0, 0], moves)//,[ 'Ryu', 'Ken', 'Chun Li', 'E.Honda', 'Ryu' ]);
+// console.log(streetFighterSelection(fighters, [0, 0], moves))//,[ 'Ryu', 'Ken', 'Chun Li', 'E.Honda', 'Ryu' ]);
 
-// moves = ['up', 'up','up'];
-// streetFighterSelection(fighters, [0, 3], moves)//,[ 'Ryu', 'Ken', 'Chun Li', 'E.Honda', 'Ryu' ]);
+// moves = ['up', 'up', 'up'];
+// console.log(streetFighterSelection(fighters, [0, 3], moves))//,[ 'Guile', 'Guile', 'Guile' ]
 
-moves = ["up","left","down","right","up","left","down","right"];
-streetFighterSelection(fighters, [0,0], moves)//,['Ryu', 'Vega', 'M.Bison', 'Ken', 'Ryu', 'Vega', 'M.Bison', 'Ken'];
+// moves = ["up", "left", "down", "right", "up", "left", "down", "right"];
+// console.log(streetFighterSelection(fighters, [0, 0], moves))//,['Ryu', 'Vega', 'M.Bison', 'Ken', 'Ryu', 'Vega', 'M.Bison', 'Ken'];
